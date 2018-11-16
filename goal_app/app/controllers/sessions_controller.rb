181bee69f437f366aa834acf26ad81133053e4f2
anthonykumasaka <anthonykumasaka@gmail.com>
2018-11-16 14:41:47 -0800

    Add additional specs

--- goal_app/app/controllers/sessions_controller.rb
+++ goal_app/app/controllers/sessions_controller.rb
@@ -1,3 +1,22 @@
 class SessionController < ApplicationController
+  def new
+    render :new
+  end
   
+  def create
+    user = User.find_by_credentials(params[:user][:username], params[:user][:username])
+    
+    if user
+      login!(user)
+      redirect_to links_url
+    else
+      flash.now[:errors] = ['Invalid credentials']
+      render :new
+    end
+  end
+  
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
 end
